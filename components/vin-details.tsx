e0a4ef45d3a92095379db6b7555725f040ab82d8
Alex Bond <alexb@updg.net>
2021-10-13 20:21:35 -0500

    Notes

--- components/vin-details.tsx
+++ components/vin-details.tsx
@@ -51,41 +51,67 @@ export const VinDetails: React.FC<{ vin: string }> = (props) => {
         <>
           <h2>Results for VIN {vin}</h2>
           {response?.data?.technicalCampaigns?.length ? (
-            <table className='table'>
-              <thead>
-              <tr>
-                <th>Recall Type</th>
-                <th>Defect Code</th>
-                <th>Defect Code Description</th>
-                <th>Reservation Date</th>
-                <th>Vin Status</th>
-                <th>Reservation Dealer</th>
-                <th>Customer Notification</th>
-                <th>Error Flag</th>
-                <th>Error Message</th>
-              </tr>
-              </thead>
-              <tbody>
-              {response?.data.technicalCampaigns.map(item => (
-                <tr key={item.defectCode}>
-                  <td>{item.recallType}</td>
-                  <td>{item.defectCode}</td>
-                  <td>{item.defectCodeDescription?.length ? item.defectCodeDescription : DefectCodeInfo[item.defectCode]?.defectCodeDescription}</td>
-                  <td>{item.reservationDate}</td>
-                  <td>{item.vinStatus}</td>
-                  <td>{item.reservationDealer}</td>
-                  <td>{item.customerNotification}</td>
-                  <td>{item.ErrorFlag}</td>
-                  <td>{item.ErrorMessage}</td>
+            <>
+              <table className='table'>
+                <thead>
+                <tr>
+                  <th>Recall Type</th>
+                  <th>Defect Code</th>
+                  <th>Defect Code Description</th>
+                  <th>Reservation Date</th>
+                  <th>VIN Status</th>
+                  <th>Reservation Dealer</th>
+                  <th>Customer Notification</th>
+                  <th>Error Flag</th>
+                  <th>Error Message</th>
                 </tr>
-              ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                {response?.data.technicalCampaigns.map(item => (
+                  <tr key={item.defectCode}>
+                    <td>{item.recallType}</td>
+                    <td>{item.defectCode}</td>
+                    <td>{item.defectCodeDescription?.length ? item.defectCodeDescription : DefectCodeInfo[item.defectCode]?.defectCodeDescription}</td>
+                    <td>{item.reservationDate}</td>
+                    <td>{item.vinStatus}</td>
+                    <td>{item.reservationDealer}</td>
+                    <td>{item.customerNotification}</td>
+                    <td>{item.ErrorFlag}</td>
+                    <td>{item.ErrorMessage}</td>
+                  </tr>
+                ))}
+                </tbody>
+              </table>
+            </>
           ) : (
             <h3 className='my-5'>No open issues found</h3>
           )}
 
           <Link href='/'><a className='btn btn-primary btn-block'>Try different VIN</a></Link>
+
+          <hr />
+          <h4>Couple notes:</h4>
+          <ul>
+            <li>
+              This tool uses BMW GROUP API, which means that data comes from global database. Tool by itself is not
+              official, so that that data can be outdated or not complete. If you have questions about your car, please
+              contact BMW Genius at 1.844.4GENIUS (443-6487) or your local dealer.
+            </li>
+            <li>
+              <strong>Recall Type</strong> can have a value of <code>no recall</code>, which usually means that this
+              is <strong>enhancement</strong> (thanks to the great chip shortages) when a car is not yet delivered to
+              the customer.
+            </li>
+            <li>
+              <strong>VIN Status</strong> can be <code>open</code> which means that issue is not yet resolved
+              or <code>closed</code> for fixed items.
+            </li>
+            <li>
+              <strong>Code Descriptions</strong> was removed from API recently, but I'm trying to map known defect codes
+              to descriptions. If you don't see a description for specific code but know what it is, please submit the
+              issue on GitHub, and I will add it to the tool.
+            </li>
+          </ul>
         </>
       )
     } else {
