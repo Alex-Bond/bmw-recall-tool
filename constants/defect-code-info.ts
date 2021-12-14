7cc9427b021068bf83e09a6397a49911fa7f79fa
Alex Bondarevskyi <me@alexbond.info>
2021-12-14 17:18:54 -0600

    0061230600 - small description change

--- constants/defect-code-info.ts
+++ constants/defect-code-info.ts
@@ -24,7 +24,7 @@ export const DefectCodeInfo: { [key: string]: { defectCodeDescription: string } 
     defectCodeDescription: 'Checking the installation of amplifier, retrofitting if necessary (note: not confirmed message)',
   },
   '0061230600': {
-    defectCodeDescription: 'Vehicle battery insert check',
+    defectCodeDescription: 'Inspect The Vehicle Battery Insert',
   },
   '0084860100': {
     defectCodeDescription: 'Program Telematics Control Unit (ATM)',
