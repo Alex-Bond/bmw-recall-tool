94ea2b424437b6b9217c805cc7cee30bedb427f5
Alex Bondarevskyi <me@alexbond.info>
2023-09-27 19:22:32 -0500

    Fix keys

--- netlify/functions/vin.ts
+++ netlify/functions/vin.ts
@@ -2,7 +2,7 @@ import { Handler } from '@netlify/functions'
 import axios from 'axios'
 
 const CryptoJS = require('node-cryptojs-aes').CryptoJS
-const accessToken = 'U3VyeWE6STJCVklDR1dnb2p4YjRkQg=='
+const accessToken = 'VmVoaWNsZVJlY2FsbFVJOkkyQlZJQ0dXZ29qeGI0ZEI='
 
 const handler: Handler = async (event, context) => {
   if (event.httpMethod != 'POST') return { statusCode: 200 }
@@ -14,9 +14,10 @@ const handler: Handler = async (event, context) => {
     }
   }>(`https://vehiclerecall.bmwgroup.com/api/recall?vin=${vin}&market=GB&language=EN`, {
     headers: {
-      Authorization: `Basic ${CryptoJS.AES.encrypt(accessToken.trim(), 'hdlurn13bjdlnvld').toString()}`,
+      Authorization: `Basic ${CryptoJS.AES.encrypt(accessToken.trim(), 'vren!@noswa').toString()}`,
       Referer: 'https://vehiclerecall.bmwgroup.com/index.html?brand=bmw&market=uk&language=en',
       isHitFromUI: 'true',
+      FromUI: 'true',
       'X-Frame-Options': 'deny',
       'Content-Type': 'application/json',
       Accept: 'application/json',
