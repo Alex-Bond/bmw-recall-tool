072efc06601197fcd336d2d5b13b5de3aa724901
itsanalogue <139643481+itsanalogue@users.noreply.github.com>
2024-05-31 09:32:20 -0500

    sort by keys

--- constants/defect-code-info.ts
+++ constants/defect-code-info.ts
@@ -1,225 +1,45 @@
 export const DefectCodeInfo: { [key: string]: { defectCodeDescription: string } } = {
-  '0066540100': {
-    defectCodeDescription: 'Programming control units (front radar sensor)',
+  '0011050700': {
+    defectCodeDescription: 'Replacing exhaust-gas recirculation cooler/ Sales Lock',
   },
-  '0066530100': {
-    defectCodeDescription: 'Checking the installation of camera, retrofitting if necessary',
+  '0011150700': {
+    defectCodeDescription: 'Replacing the exhaust gas recirculation cooler (high temperature)/ Sales Lock',
   },
-  '0061070600': {
-    defectCodeDescription: 'Program control units (BDC)',
+  '0011200700': {
+    defectCodeDescription: 'Replacing exhaust gas recirculation cooler/ Sales Lock',
   },
-  '0065480300': {
-    defectCodeDescription: 'Retrofitting the receiver audio module',
+  '0011240600': {
+    defectCodeDescription: 'Replace the Main Bearing Shells',
   },
-  'STOP021954': {
-    defectCodeDescription: 'Fxx Gxx B47 B57 B58 S58 - Main Bearing Shell',
+  '0011350700': {
+    defectCodeDescription: 'Exhaust gas recirculation cooler customer notification',
   },
-  '0066570100': {
-    defectCodeDescription: 'Checking the installation of the camera and TRSVC control unit, retrofitting if necessary',
+  '0011460700': {
+    defectCodeDescription: 'G0x G70 Replacing switchable rocker arm exhaust',
+  },
+  '0011570600': {
+    defectCodeDescription: 'Replacing the exhaust gas recirculation cooler (high temperature)/ Sales Lock',
   },
   '0012610500': {
     defectCodeDescription: 'Program control units (DME)',
   },
-  '0065600300': {
-    defectCodeDescription: 'Checking the installation of amplifier, retrofitting if necessary (note: not confirmed message)',
-  },
-  '0061230600': {
-    defectCodeDescription: 'Inspect The Vehicle Battery Insert',
-  },
-  '0084860100': {
-    defectCodeDescription: 'Program Telematics Control Unit (ATM)',
-  },
-  '0011240600': {
-    defectCodeDescription: 'Replace the Main Bearing Shells'
-  },
-  '10065640300': {
-    defectCodeDescription: 'Programming Control Units (CID) Delivery Stop'
-  },
-  '0066640100': {
-    defectCodeDescription: 'Retrofit PMA Control Unit'
-  },
   '0012690500': {
-    defectCodeDescription: 'Program the Digital Motor Electronics (MDE)'
+    defectCodeDescription: 'Program the Digital Motor Electronics (DME)',
   },
-  '0065740300': {
-    defectCodeDescription: 'Programming control units (head unit)'
+  '0012890500': {
+    defectCodeDescription: 'Program Control Unit (DME)',
   },
-  '0065830300': {
-    defectCodeDescription: 'Programming control units (head unit)'
+  '0012940500': {
+    defectCodeDescription: 'Electric Front Axle Drive Unit',
   },
-  '0065820300': {
-    defectCodeDescription: 'Recall: Valet Parking Mode - Programming control units (head unit)'
-  },
-  '0061880600': {
-    defectCodeDescription: 'Checking the high-voltage connection field and replacing it if necessary'
-  },
-  '0061860600': {
-    defectCodeDescription: 'Retrofitting the standard charging cable (mode 2) for domestic socket outlets'
-  },
-  '0065630300': {
-    defectCodeDescription: 'Retrofitting the central information display'
-  },
-  '0084980100': {
-    defectCodeDescription: 'Programming the control units (quality measures)'
-  },
-  '0065750300': {
-    defectCodeDescription: 'Programming control units (airbag control unit)'
-  },
-  '0065780300': {
-    defectCodeDescription: 'Programming control units part II (airbag control unit)'
-  },
-  'INFO123825': {
-    defectCodeDescription: 'I20 - ADCAM low Zertifikatefehler bei Programmierung'
-  },
-  '0065800300': {
-    defectCodeDescription: 'Programming control units (head unit)'
-  },
-  '0065810300': {
-    defectCodeDescription: 'Programming control units (head unit)'
-  },
-  '0032350300': {
-    defectCodeDescription: 'G26 Program Control Unit (software update)'
-  },
-  '0061030700': {
-    defectCodeDescription: 'G26 Body Domain Controller unit replacement'
-  },
-  '0061960600': {
-    defectCodeDescription: 'Recall: High-Voltage Battery'
-  },
-  '0028110100': {
-    defectCodeDescription: 'Programming control units (DKG)'
-  },
-  '0084140200': {
-    defectCodeDescription: 'Programming the telematics control unit',
-  },
-  '0061400700': {
-    defectCodeDescription: 'Program Control Units (SME) - battery management electronics module',
-  },
-  '0041160200': {
-    defectCodeDescription: 'Check the roofing sheet for bonding',
+  '0013510300': {
+    defectCodeDescription: 'Program control units (DDE)',
   },
   '0013850300': {
     defectCodeDescription: 'Program control units (DDE) due to deposits in the intake system',
   },
-  '0061640400': {
-    defectCodeDescription: 'Program control units (GWS)',
-  },
-  '0065980300': {
-    defectCodeDescription: 'Booster Control Unit',
-  },
-  '0061100800': {
-    defectCodeDescription: 'Wiper mechanism recall'
-  },
-  '0032470300': {
-    defectCodeDescription: 'Steering wheel ground connection recall'
-  },
-  '0052130200': {
-    defectCodeDescription: 'Seat rail recall'
-  },
-  '0065210400': {
-    defectCodeDescription: 'Program control units (head unit)'
-  },
-  '0061850700': {
-    defectCodeDescription: 'Program control units (SME)'
-  },
-  '0012940500': {
-    defectCodeDescription: 'Electric Front Axle Drive Unit'
-  },
-  '0012890500': {
-    defectCodeDescription: 'Program Control Unit (DME)'
-  },
-  '0061020800': {
-    defectCodeDescription: 'Verify cable bundle in the bumper'
-  },
-  '10011350700': {
-    defectCodeDescription: 'Exhaust gas recirculation cooler customer notification',
-  },
-  '10011570600': {
-    defectCodeDescription: 'Replacing the exhaust gas recirculation cooler (high temperature)/ Sales Lock',
-  },
-  '10011150700': {
-    defectCodeDescription: 'Replacing the exhaust gas recirculation cooler (high temperature)/ Sales Lock',
-  },
-  '10011050700': {
-    defectCodeDescription: 'Replacing exhaust-gas recirculation cooler/ Sales Lock',
-  },
-  '10011200700': {
-    defectCodeDescription: 'Replacing exhaust gas recirculation cooler/ Sales Lock',
-  },
-  '10061050500': {
-    defectCodeDescription: 'Installing the repair cable for the positive battery terminal line/ Sales Lock',
-  },
-  '10032740200': {
-    defectCodeDescription: 'Reworking steering gear',
-  },
-  '10061830400': {
-    defectCodeDescription: 'Checking, if required, reworking plug connection of blower controller (Part II)',
-  },
-  '1STOP024773': {
-    defectCodeDescription: 'Compliance Examination',
-  },
-  '10064060200': {
-    defectCodeDescription: 'Optimising the water drain',
-  },
-  '10013850300': {
-    defectCodeDescription: 'Program control units (DDE)',
-  },
-  '10013510300': {
-    defectCodeDescription: 'Program control units (DDE)',
-  },
-  '10032430300': {
-    defectCodeDescription: 'Replace driver\'s airbag (PSDI-5)',
-  },
-  '10072500200': {
-    defectCodeDescription: 'Replacing front passenger airbag (PSPI-2)',
-  },
-  '0065020400': {
-    defectCodeDescription: 'Program control units (booster)',
-  },
-  '0032400300': {
-    defectCodeDescription: 'Replace driver\'s airbag',
-  },
-  '0061390700': {
-    defectCodeDescription: 'Program control units (SME)',
-  },
-  '0061630700': {
-    defectCodeDescription: 'Program control units (seat module)',
-  },
-  '0032450300': {
-    defectCodeDescription: 'Program control units (MFL)',
-  },
-  '0061540700': {
-    defectCodeDescription: 'Program control units (seat module)',
-  },
-  '0084900100': {
-    defectCodeDescription: 'Program control units',
-  },
-  '0065770400': {
-    defectCodeDescription: 'I20 Programming control units (quality measures)',
-  },
-  '0032540300': {
-    defectCodeDescription: 'I20 Replace steering gear',
-  },
-  '0065680400': {
-    defectCodeDescription: 'U10 U11 Updating head unit apps',
-  },
-  '0065690400': {
-    defectCodeDescription: 'F66 J25 Updating head unit apps',
-  },
-  '0065740400': {
-    defectCodeDescription: 'U10 U11 Program control units (quality measures)',
-  },
-  '0065750400': {
-    defectCodeDescription: 'U25 Program control units (quality measures)',
-  },
-  '0065760400': {
-    defectCodeDescription: 'U10 U11 G05 G06 G07 G09 G60 G70 Program control units (quality measures)',
-  },
-  '0064290200': {
-    defectCodeDescription: 'I20 Replace coolant changeover valve',
-  },
-  '0064280200': {
-    defectCodeDescription: 'Gx BEV Replace coolant changeover valve',
+  '0028110100': {
+    defectCodeDescription: 'Programming control units (DKG)',
   },
   '0028230100': {
     defectCodeDescription: 'U11 Reworking DKG connection - transfer box',
@@ -227,20 +47,50 @@ export const DefectCodeInfo: { [key: string]: { defectCodeDescription: string } 
   '0028240100': {
     defectCodeDescription: 'U10 U11 Reworking DKG connection - transfer box',
   },
-  '0011460700': {
-    defectCodeDescription: 'G0x G70 Replacing switchable rocker arm exhaust',
+  '0032350300': {
+    defectCodeDescription: 'G26 Program Control Unit (software update)',
   },
-  '0065230400': {
-    defectCodeDescription: 'BMW Operating System 8.5',
+  '0032400300': {
+    defectCodeDescription: "Replace driver's airbag",
   },
-  '0065240400': {
-    defectCodeDescription: 'I20 BMW Operating System 8.5',
+  '0032430300': {
+    defectCodeDescription: "Replace driver's airbag (PSDI-5)",
   },
-  '0064230200': {
-    defectCodeDescription: 'Gx Coolant changeover valve',
+  '0032450300': {
+    defectCodeDescription: 'Program control units (MFL)',
   },
-  '0064240200': {
-    defectCodeDescription: 'I20 Coolant changeover valve',
+  '0032470300': {
+    defectCodeDescription: 'Steering wheel ground connection recall',
+  },
+  '0032540300': {
+    defectCodeDescription: 'I20 Replace steering gear',
+  },
+  '0032740200': {
+    defectCodeDescription: 'Reworking steering gear',
+  },
+  '0041160200': {
+    defectCodeDescription: 'Check the roofing sheet for bonding',
+  },
+  '0051520500': {
+    defectCodeDescription: 'I20 Replace cover trim with radome heating',
+  },
+  '0052130200': {
+    defectCodeDescription: 'Seat rail recall',
+  },
+  '0061020800': {
+    defectCodeDescription: 'Verify cable bundle in the bumper',
+  },
+  '0061030700': {
+    defectCodeDescription: 'G26 Body Domain Controller unit replacement',
+  },
+  '0061050500': {
+    defectCodeDescription: 'Installing the repair cable for the positive battery terminal line/ Sales Lock',
+  },
+  '0061070600': {
+    defectCodeDescription: 'Program control units (BDC)',
+  },
+  '0061100800': {
+    defectCodeDescription: 'Wiper mechanism recall',
   },
   '0061140800': {
     defectCodeDescription: 'I20 Program Combined Charging Unit (CCU)',
@@ -251,8 +101,17 @@ export const DefectCodeInfo: { [key: string]: { defectCodeDescription: string } 
   '0061170700': {
     defectCodeDescription: 'I20 Replacing the Combined Charging Unit (CCU)',
   },
+  '0061230600': {
+    defectCodeDescription: 'Inspect The Vehicle Battery Insert',
+  },
   '0061250800': {
     defectCodeDescription: 'Gx Replacing Combined Charging Unit',
+  },
+  '0061390700': {
+    defectCodeDescription: 'Program control units (SME)',
+  },
+  '0061400700': {
+    defectCodeDescription: 'Program Control Units (SME) - battery management electronics module',
   },
   '0061460800': {
     defectCodeDescription: 'G26 Replace Combined Charging Unit (CCU)',
@@ -263,34 +122,154 @@ export const DefectCodeInfo: { [key: string]: { defectCodeDescription: string } 
   '0061500700': {
     defectCodeDescription: 'G09 G70 Program PHEV control unit (CCU)',
   },
+  '0061540700': {
+    defectCodeDescription: 'Program control units (seat module)',
+  },
+  '0061630700': {
+    defectCodeDescription: 'Program control units (seat module)',
+  },
+  '0061640400': {
+    defectCodeDescription: 'Program control units (GWS)',
+  },
+  '0061830400': {
+    defectCodeDescription: 'Checking, if required, reworking plug connection of blower controller (Part II)',
+  },
+  '0061850700': {
+    defectCodeDescription: 'Program control units (SME)',
+  },
+  '0061860600': {
+    defectCodeDescription: 'Retrofitting the standard charging cable (mode 2) for domestic socket outlets',
+  },
+  '0061880600': {
+    defectCodeDescription: 'Checking the high-voltage connection field and replacing it if necessary',
+  },
   '0061890700': {
     defectCodeDescription: 'Replace Combined Charging Unit (CCU)',
   },
   '0061900700': {
     defectCodeDescription: 'Replace Combined Charging Unit (CCU)',
   },
-  '0051520500': {
-    defectCodeDescription: 'I20 Replace cover trim with radome heating',
+  '0061960600': {
+    defectCodeDescription: 'Recall: High-Voltage Battery',
+  },
+  '0064060200': {
+    defectCodeDescription: 'Optimising the water drain',
+  },
+  '0064230200': {
+    defectCodeDescription: 'Gx Coolant changeover valve',
+  },
+  '0064240200': {
+    defectCodeDescription: 'I20 Coolant changeover valve',
+  },
+  '0064280200': {
+    defectCodeDescription: 'Gx BEV Replace coolant changeover valve',
+  },
+  '0064290200': {
+    defectCodeDescription: 'I20 Replace coolant changeover valve',
+  },
+  '0065020400': {
+    defectCodeDescription: 'Program control units (booster)',
+  },
+  '0065210400': {
+    defectCodeDescription: 'Program control units (head unit)',
+  },
+  '0065230400': {
+    defectCodeDescription: 'BMW Operating System 8.5',
+  },
+  '0065240400': {
+    defectCodeDescription: 'I20 BMW Operating System 8.5',
+  },
+  '0065480300': {
+    defectCodeDescription: 'Retrofitting the receiver audio module',
+  },
+  '0065600300': {
+    defectCodeDescription: 'Checking the installation of amplifier, retrofitting if necessary (note: not confirmed message)',
+  },
+  '0065630300': {
+    defectCodeDescription: 'Retrofitting the central information display',
+  },
+  '0065640300': {
+    defectCodeDescription: 'Programming Control Units (CID) Delivery Stop',
+  },
+  '0065680400': {
+    defectCodeDescription: 'U10 U11 Updating head unit apps',
+  },
+  '0065690400': {
+    defectCodeDescription: 'F66 J25 Updating head unit apps',
+  },
+  '0065740300': {
+    defectCodeDescription: 'Programming control units (head unit)',
+  },
+  '0065740400': {
+    defectCodeDescription: 'U10 U11 Program control units (quality measures)',
+  },
+  '0065750300': {
+    defectCodeDescription: 'Programming control units (airbag control unit)',
+  },
+  '0065750400': {
+    defectCodeDescription: 'U25 Program control units (quality measures)',
+  },
+  '0065760400': {
+    defectCodeDescription: 'U10 U11 G05 G06 G07 G09 G60 G70 Program control units (quality measures)',
+  },
+  '0065770400': {
+    defectCodeDescription: 'I20 Programming control units (quality measures)',
+  },
+  '0065780300': {
+    defectCodeDescription: 'Programming control units part II (airbag control unit)',
+  },
+  '0065800300': {
+    defectCodeDescription: 'Programming control units (head unit)',
+  },
+  '0065810300': {
+    defectCodeDescription: 'Programming control units (head unit)',
+  },
+  '0065820300': {
+    defectCodeDescription: 'Recall: Valet Parking Mode - Programming control units (head unit)',
+  },
+  '0065830300': {
+    defectCodeDescription: 'Programming control units (head unit)',
+  },
+  '0065980300': {
+    defectCodeDescription: 'Booster Control Unit',
+  },
+  '0066530100': {
+    defectCodeDescription: 'Checking the installation of camera, retrofitting if necessary',
+  },
+  '0066540100': {
+    defectCodeDescription: 'Programming control units (front radar sensor)',
+  },
+  '0066570100': {
+    defectCodeDescription: 'Checking the installation of the camera and TRSVC control unit, retrofitting if necessary',
+  },
+  '0066640100': {
+    defectCodeDescription: 'Retrofit PMA Control Unit',
+  },
+  '0072500200': {
+    defectCodeDescription: 'Replacing front passenger airbag (PSPI-2)',
+  },
+  '0084140200': {
+    defectCodeDescription: 'Programming the telematics control unit',
   },
   '0084230200': {
     defectCodeDescription: 'F9x G08 G1x G29 G42 G87 Program control units and include Digital Key (wireless charging tray)',
   },
-  '0034670200': {
-    defectCodeDescription: 'Fx Gx Ux Replacing integrated braking system',
+  '0084860100': {
+    defectCodeDescription: 'Program Telematics Control Unit (ATM)',
   },
-  '0064310200': {
-    defectCodeDescription: 'G05 G09 Reworking heater circuit coolant hoses',
+  '0084900100': {
+    defectCodeDescription: 'Program control units',
   },
-  '0072630200': {
-    defectCodeDescription: 'Recall 24V-345: Front lower safety belt',
+  '0084980100': {
+    defectCodeDescription: 'Programming the control units (quality measures)',
   },
-  '0012200600': {
-    defectCodeDescription: 'Valvetronic - Program control units (DME)',
+  INFO123825: {
+    defectCodeDescription: 'I20 - ADCAM low Zertifikatefehler bei Programmierung',
   },
-  '0061330800': {
-    defectCodeDescription: 'G05 PHEV Checking and if necessary, reworking inverter supply line',
+  STOP021954: {
+    defectCodeDescription: 'Fxx Gxx B47 B57 B58 S58 - Main Bearing Shell',
   },
-  '0084300200': {
-    defectCodeDescription: 'Replace wireless charging tray',
+  STOP024773: {
+    defectCodeDescription: 'Compliance Examination',
   },
-}
+};
